4e57eced3381aedbd71eeae02179eaa5bc44b8b6
lookcw <lookwchristopher@gmail.com>
2020-08-21 14:52:35 -0400

    restructuring folder

--- EatIn/settings.py
+++ EatIn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,3 +148,4 @@ CORS_ORIGIN_WHITELIST = [
 
 CSRF_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
+django_heroku.settings(locals())
